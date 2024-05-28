9f3383bcd88f9d61df703806065b340468880a55
Aamir Mahboob <amirmehboob16@gmail.com>
2024-05-28 11:12:40 +0500

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,31 @@
-import { Routes, Route, Navigate, useLocation } from "react-router-dom";
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import { Dashboard, Auth } from "@/layouts";
-import PrivateRoute from './PrivateRoute';
+import PrivateRoute from './PrivateRoute' 
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { useAuth } from './context/AuthContext';
+import { useAuth } from "./context/AuthContext";
 import { useEffect } from "react";
 
 function App() {
-  const { currentUser } = useAuth();
-  const location = useLocation();
-
-  useEffect(() => {
-    if (currentUser && location.pathname === "/") {
-      // Redirect to the expenseandincome page if the user is logged in and the current path is "/"
-      return <Navigate to="/dashboard/expenseandincome" />;
+   const {currentUser} = useAuth()
+   const navigate =  useNavigate()
+   useEffect(() => {
+    if (currentUser) {
+      navigate('/dashboard/expenseandincome');
     }
-  }, [currentUser, location.pathname]);
+  }, [currentUser, navigate]);
 
   return (
     <>
-      <Routes>
-        <Route path="/auth/*" element={<Auth />} />
-        <Route path="/dashboard/*" element={<PrivateRoute element={<Dashboard />} />} />
-        <Route path="*" element={<Navigate to="/auth/sign-in" />} />
-      </Routes>
-      <ToastContainer />
-    </>
+    <Routes>
+      <Route path="/auth/*" element={<Auth />} />
+      <Route path="/dashboard/*" element={<PrivateRoute element={<Dashboard />} />} />
+      <Route path="*" element={<Navigate to="/auth/sign-in" />} />
+    </Routes>
+     <ToastContainer />
+  </>
   );
+
 }
 
 export default App;
